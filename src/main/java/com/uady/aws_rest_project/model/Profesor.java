d667c5475728a1c128c93bc0bb3d46102e0a8ba2
Alvaro Xool <alvaroxool14@gmail.com>
2025-12-07 15:38:47 -0600

    add database support using MySQL

--- src/main/java/com/uady/aws_rest_project/model/Profesor.java
+++ src/main/java/com/uady/aws_rest_project/model/Profesor.java
@@ -1,17 +1,72 @@
 package com.uady.aws_rest_project.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "professors")
+
 public class Profesor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Integer id;
+
+    @Column(name="numero_empleado")
     private Integer numeroEmpleado;
+
+    @Column(name="nombres")
     private String nombres;
+
+    @Column(name="apellidos")
     private String apellidos;
+
+    @Column(name="horas_clase")
     private Integer horasClase;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNumeroEmpleado() {
+        return numeroEmpleado;
+    }
+
+    public void setNumeroEmpleado(Integer numeroEmpleado) {
+        this.numeroEmpleado = numeroEmpleado;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Integer getHorasClase() {
+        return horasClase;
+    }
+
+    public void setHorasClase(Integer horasClase) {
+        this.horasClase = horasClase;
+    }
 
     @Override
     public String toString() {
--- src/main/java/com/uady/aws_rest_project/model/Student.java
+++ src/main/java/com/uady/aws_rest_project/model/Student.java
@@ -1,18 +1,69 @@
 package com.uady.aws_rest_project.model;
 
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "students")
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Integer id;
+
+    @Column(name="nombres")
     private String nombres;
+
+    @Column(name="apellidos")
     private String apellidos;
+
+    @Column(name="matricula")
     private String matricula;
+
+    @Column(name="promedio")
     private Double promedio;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public Double getPromedio() {
+        return promedio;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setPromedio(Double promedio) {
+        this.promedio = promedio;
+    }
 
     @Override
     public String toString() {
